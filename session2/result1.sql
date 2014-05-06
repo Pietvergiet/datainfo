b31b25df3f481665c18ec9d3292333137776e62a
Pietvergiet <frank-bruggink@hotmail.com>
2014-05-06 15:13:36 +0200

    Result1

---
+++ session2/result1.sql
@@ -0,0 +1,11 @@
+INSERT INTO Problem1 (task, result, motivation) VALUES 
+('a', 0, "Er kunnen meerdere voertuigen met dezelfde capaciteit zijn"),
+('b', 1, "zie punt 3."),
+('c', 0, "factuur kan meerder pakjes bevatten"),
+('d', 1, "als je P weet weet je F (punt c), als je F weet weet je A (punt 8)"),
+('e', 1, "je P weet weet je O (punt 4), als je P weet weet je ook G (punt 5)"),
+('f', 0, "een bezorger kan voor meerde voertuigen gerechtigd zijn"),
+('g', 0, "een voertuig kan meerdere gemachtigde bezorgers hebben"),
+('h', 0, "Voertuig is geen key voor bestuurders"),
+('i', 1, "Voertuig is wel een key voor Capaciteit"),
+('j', 0, "Voertuig is geen key voor de combinatie Capaciteit Bestuurder gezien 1 voertuig meerdere bestuurders kan hebben");
