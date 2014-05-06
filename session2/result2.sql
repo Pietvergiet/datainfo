8528573e48cdda93599e5b75ed8431aaae72c762
Pietvergiet <frank-bruggink@hotmail.com>
2014-05-06 15:43:42 +0200

    result2 taak b

--- session2/result2.sql
+++ session2/result2.sql
@@ -2,3 +2,12 @@ INSERT INTO F (x, y, violation, motivation)  VALUES
 ('E', 'D', 1, "Als je E weet weet je alleen D en A; E -> EDA"),
 ('D', 'A', 1, "Als je D weet weet je alleen A; D -> DA")
 ;
+
+INSERT INTO F1 (x, y, violation, motivation) VALUES
+('E', 'D', 0, "")
+;
+
+INSERT INTO F2 (x, y, violation, motivation) VALUES
+('ABC', 'EF', 0, ""),
+('E', 'A', 1, "Als je E weet weet je alleen A; E -> A")
+;
